3a3d41fdf4e47d0454995392b5246f67f6142ec6
Mariya Frantsava <53209068+mariyafrantsava@users.noreply.github.com>
2021-05-17 16:51:03 +0300

    feat: add pattern of slide

--- src/dataPictures.js
+++ src/dataPictures.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Slide from '../src/components/slide';
 const IMAGE_PATH = 'public/assets/images/';
 
 // const dataPictures =  [
@@ -9,7 +10,7 @@ const IMAGE_PATH = 'public/assets/images/';
 
 const dataPictures =  [
     { id: 0, src: <div><img src="public/assets/images/img_woods_wide.jpg"/><p>yyugygy</p></div>, display: true },
-    { id: 1, src: 'img_5terre.jpg', display: false },
+    { id: 1, src: <Slide />, display: false },
     { id: 2, src: 'img_nature_wide.jpg', display: false },
 ];
 
--- webpack.config.js
+++ webpack.config.js
@@ -46,7 +46,7 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            title: "Webpack App",
+            title: "Carousel",
             buildTime: new Date().toISOString(),
             template: "public/index.html"
         }),
